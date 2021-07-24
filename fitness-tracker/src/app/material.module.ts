06b055d737db97493b37ec69a867fa844cf34df2
Jae No <jaeyno@gmail.com>
2021-07-24 17:06:41 -0400

    [Training Component] add a card

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -11,6 +11,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [],
@@ -27,6 +28,7 @@ import {MatTabsModule} from '@angular/material/tabs';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
@@ -40,6 +42,7 @@ import {MatTabsModule} from '@angular/material/tabs';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
   ]
 })
 export class MaterialModule { }
