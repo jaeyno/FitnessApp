3d39fd9f16e50188e1cd16f2c37510c79f50df0b
Jae No <jaeyno@gmail.com>
2021-08-08 05:05:06 -0400

    [Pre-deploy] insert firebase settings for the production

--- fitness-tracker/src/environments/environment.prod.ts
+++ fitness-tracker/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAiG6rsBCOGTubE0wcpa7KlDrIvGF3tIRA",
+    authDomain: "ng-fitness-tracker-70069.firebaseapp.com",
+    projectId: "ng-fitness-tracker-70069",
+    storageBucket: "ng-fitness-tracker-70069.appspot.com",
+    messagingSenderId: "543930861251",
+    appId: "1:543930861251:web:2142c71bba22bde6220476",
+    measurementId: "G-W19G742SJ6"
+  }
 };
