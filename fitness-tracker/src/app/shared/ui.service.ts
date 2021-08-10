e2177099c2851600dda74a30b2f062553e59cf82
Jae No <jaeyno@gmail.com>
2021-08-10 10:34:46 -0400

    [Shared Module] remove all the unnecessary code

--- fitness-tracker/src/app/shared/ui.service.ts
+++ fitness-tracker/src/app/shared/ui.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UiService {
-
-  //loadingStateChanged = new Subject<boolean>();
 
   constructor(private snackBarService: MatSnackBar) { }
 
